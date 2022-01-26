c8ebd782a8dcdfa31ccbd61a1168d887f4956537
Daniel <seilerdaniel86@gmail.com>
2022-01-25 22:52:52 -0300

    Agregado Ejemplos de Hooks de React

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Ejemplo1 from './hooks/Ejemplo1';
+import Ejemplo2 from './hooks/Ejemplo2';
+import MiComponenteConContexto from './hooks/Ejemplo3';
+
+import Ejemplo4 from './hooks/Ejemplo4';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        {/* <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
@@ -16,7 +22,17 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
+        {/* Ejemplos de uso de HOOKS */}
+        {/* <Ejemplo1></Ejemplo1> */}
+        {/* <Ejemplo2></Ejemplo2> */}
+        {/* <MiComponenteConContexto></MiComponenteConContexto> */}
+        <Ejemplo4 nombre="Daniel">
+          {/* Todo lo que hay aquí, es tratado como props.children */}
+          <h3>
+            Contenido del props.children
+          </h3>
+        </Ejemplo4>
       </header>
     </div>
   );
