e69b4fa99ee67b6587794ee5d76c74ede5fdd60c
Daniel <seilerdaniel86@gmail.com>
2022-01-26 18:43:53 -0300

    Ejemplos de Hooks y Aplicar Estilos CSS

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Greeting from "./components/pure/greeting";
+import Greetingf from "./components/pure/greetingF";
+import TaskListComponent from "./components/container/task_list";
 import Ejemplo1 from './hooks/Ejemplo1';
 import Ejemplo2 from './hooks/Ejemplo2';
 import MiComponenteConContexto from './hooks/Ejemplo3';
-
 import Ejemplo4 from './hooks/Ejemplo4';
 
 
@@ -12,27 +14,22 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        {/* <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
+        {/* Componente propio Greeting.jsx */}
+        {/* <Greeting name={"Daniel"}></Greeting> */}
+        {/* Componente de ejemplo funcional */}
+        {/* <Greetingf name="Daniel"></Greetingf> */}
+        {/* Componente de Listado de Tareas */}
+        <TaskListComponent></TaskListComponent>
         {/* Ejemplos de uso de HOOKS */}
         {/* <Ejemplo1></Ejemplo1> */}
         {/* <Ejemplo2></Ejemplo2> */}
         {/* <MiComponenteConContexto></MiComponenteConContexto> */}
-        <Ejemplo4 nombre="Daniel">
-          {/* Todo lo que hay aquí, es tratado como props.children */}
+        {/* Todo lo que hay aquí, es tratado como props.children */}
+        {/* <Ejemplo4 nombre="Daniel">
           <h3>
             Contenido del props.children
           </h3>
-        </Ejemplo4>
+        </Ejemplo4> */}
       </header>
     </div>
   );
---
+++ src/components/container/task_list.jsx
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+import { LEVELS } from "../../models/levels.enum";
+import { Task } from "../../models/task.class";
+import TaskComponent from "../pure/task";
+
+// Importamos la hoja de estilos de task.scss
+import '../../styles/task.scss';
+
+const TaskListComponent = () => {
+
+  const defaultTask = new Task(
+    "Daniel",
+    "Building an Task App",
+    true,
+    LEVELS.NORMAL
+  );
+
+  // Estado del componente
+  const [tasks, setTasks] = useState(defaultTask);
+  const [loading, setLoading] = useState(true);
+
+  // Control del ciclo de vida del componente
+    useEffect(() => {
+      console.log("Task State has been modified");
+      setLoading(false);
+      return () => {
+        console.log("TaskList component is going to unmount...");
+      };
+    }, [tasks]);
+
+  const changeCompleted = (id) => {
+    console.log("TODO: Change state of a task");
+  }
+
+  return (
+    <div>
+      <div>
+        <h1>Your Task:</h1>
+      </div>
+      {/* TODO: Aplicar un For/Map para renderizar una lista */}
+      <TaskComponent task={defaultTask}></TaskComponent>
+    </div>
+  )
+
+};
+
+export default TaskListComponent;
