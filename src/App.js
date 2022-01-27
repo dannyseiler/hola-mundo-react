db77ac2da3252e9b8e1d65f712980036b2f5aab5
Daniel <seilerdaniel86@gmail.com>
2022-01-26 21:14:03 -0300

    Agregado componente greetingStyled

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Ejemplo1 from './hooks/Ejemplo1';
 import Ejemplo2 from './hooks/Ejemplo2';
 import MiComponenteConContexto from './hooks/Ejemplo3';
 import Ejemplo4 from './hooks/Ejemplo4';
+import Greetingstyled from './components/pure/greetingStyled';
 
 
 function App() {
@@ -19,7 +20,7 @@ function App() {
         {/* Componente de ejemplo funcional */}
         {/* <Greetingf name="Daniel"></Greetingf> */}
         {/* Componente de Listado de Tareas */}
-        <TaskListComponent></TaskListComponent>
+        {/* <TaskListComponent></TaskListComponent> */}
         {/* Ejemplos de uso de HOOKS */}
         {/* <Ejemplo1></Ejemplo1> */}
         {/* <Ejemplo2></Ejemplo2> */}
@@ -30,6 +31,7 @@ function App() {
             Contenido del props.children
           </h3>
         </Ejemplo4> */}
+        <Greetingstyled name="Daniel"></Greetingstyled>
       </header>
     </div>
   );
